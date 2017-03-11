68ea28ead8d2c0625392067b0d8df8a3c3647953
Brian Sorahan <bsorahan@gmail.com>
2017-03-11 09:00:47 -0600

    initial commit

---
+++ addresses.go
@@ -0,0 +1,9 @@
+package cmd
+
+const (
+	AddressPulse       = "/sync/pulse"
+	AddressSlaveAdd    = "/sync/slave/add"
+	AddressSlaveList   = "/sync/slave/list"
+	AddressSlaveRemove = "/sync/slave/remove"
+	AddressTempo       = "/sync/tempo"
+)
